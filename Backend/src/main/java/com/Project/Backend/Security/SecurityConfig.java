ca39d41e39e2b3e2d658df853425d7eaeb68dbb0
AkumaSuraimu <Bolarde321@gmail.com>
2025-04-19 14:56:50 +0800

    updated CORS

--- Backend/src/main/java/com/Project/Backend/Security/SecurityConfig.java
+++ Backend/src/main/java/com/Project/Backend/Security/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource configurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://it342-lostandfoundmanagementsystem.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
